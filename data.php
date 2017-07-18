fabc656cd831a1daaad9fa06f498a5a1397d3e8c
gunchul.song <gunchul77@gmail.com>
2017-07-18 20:04:33 +1000

    add data

--- data.php
+++ data.php
@@ -13,6 +13,7 @@ $ranking = array
 	   array("서조사", "59", "2285", "59_1_seo_1.jpg", "59_1_seo_2.jpg"),
 	   array("J.HAN", "59", "2844", "59_1_J.HAN_1.jpg", "59_1_J.HAN_2.jpg"),
 	   array("두리", "58.5", "58.5_1_duri_1.jpg", "58.5_1_duri_2.jpg", "58.5_1_duri_3.jpg"),
+	   array("ALEX", "58.3", "3171", "58.3_1_ALEX_1.jpg", "58.3_1_ALEX_2.jpg"),
 	   array("서조사", "58", "1982", "58_1_seo_1.jpg"),
 	   array("두리", "58", "2837", "58_1_두리_1.jpg", "58_1_두리_2.jpg"),
 	   array("J.HAN", "58", "2893", "58_1_J.HAN_1.jpg", "58_1_J.HAN_2.jpg"),
@@ -23,32 +24,32 @@ $ranking = array
 	   array("Dexter", "56.5", "2777", "56.5_1_dexter_1.jpg", "56.5_1_dexter_2.jpg"),
 	   array("Dexter", "55.2", "2750", "56.5_1_dexter_1.jpg"),
 	   array("서조사", "55", "2761", "55_1_seo_1.jpg"),
+	   array("Terry", "55", "3073", "55_1_terry_1.jpg", "55_1_terry_2.jpg"),
+	   array("Amy.K", "54.5", "3137", "54.5_1_Amy.K_1.jpg", "54.5_1_Amy.K_2.jpg"),
 	   array("ALEX", "54", "2887", "54_1_alex_1.jpg", "54_1_alex_2.jpg"),
 	   array("서조사", "54", "3104", "54_1_서조사_1.jpg"),
+	   array("낚시초보", "54", "3158", "54_1_낚시초보_1.jpg", "54_1_낚시초보_2.jpg"),
+	   array("낚시초보", "54", "3171", "54_2_낚시초보_1.jpg", "54_2_낚시초보_2.jpg"),
 	   array("Dexter", "53.5", "2566", "53.5_1_dexter_1.jpg", "53.5_1_dexter_2.jpg"),
 	   array("Dexter", "53", "2978", "53_1_dexter_1.jpg"),
 	   array("Amy.K", "52", "2948", "52_1_Amy.K_1.jpg", "52_1_Amy.K_2.jpg"),
    ),
    array(
-       array("킹", "king/"),
-	   array("ALEX", "112", "2146", "112_1_ALEX_1.jpg", "112_1_ALEX_2.jpg"),
-	   array("J.HAN", "65", "65_1_J.HAN_1.jpg"),
-	   
-	   
-   ),
-   array(
        array("그루퍼: 블루", "blue/"),
+       array("J.HAN", "84", "3147", "84_1_J.HAN_1.jpg", "84_1_J.HAN_2.jpg"),
        array("세상끝까지", "82", "3027", "82_1_세상끝까지_1.jpg", "82_1_세상끝까지_2.jpg", "82_1_세상끝까지_3.jpg"),
        array("두리", "82", "3103", "82_1_두리_1.jpg", "82_1_두리_2.jpg"),
        array("서조사", "78", "2809", "78_1_seo_1.jpg", "78_1_seo_2.jpg"),
        array("서조사", "75", "2960", "75_1_seo_1.jpg", "75_1_seo_2.jpg"),
        array("서조사", "75", "2970", "75_2_seo_1.jpg", "75_2_seo_2.jpg"),
+       array("J.HAN", "75", "3131", "75_1_J.HAN_1.jpg", "75_1_J.HAN_2.jpg"),
        array("루이스", "72", "1956", "72_1_루이스_1.jpg", "72_1_루이스_2.jpg"),
 	   array("서조사", "72", "2502", "72_1_seo_1.jpg", "72_1_seo_2.jpg"),
 	   array("서조사", "70", "70_1_seo_1.jpg", "70_1_seo_2.jpg"),
 	   array("Dexter", "70", "2894", "70_1_Dexter_1.jpg", "70_1_Dexter_2.jpg"),
 	   array("채민", "66", "2974", "66_1_채민_1.jpg", "66_1_채민_2.jpg"),
 	   array("루이스", "65", "2340", "65_1_루이스_1.jpg", "65_1_루이스_2.jpg"),
+	   array("Dexter", "64", "3097", "64_1_Dexter_1.jpg", "64_1_Dexter_2.jpg"),
 	   array("서조사", "62", "2645", "62_1_seo_1.jpg", "62_1_seo_2.jpg"),
    ),
    array(
@@ -71,6 +72,23 @@ $ranking = array
 	   array("Amy.K", "45", "2844", "45_1_Amy.K_1.jpg", "45_1_Amy.K_2.jpg"),
    ),
    array(
+       array("킹", "king/"),
+	   array("ALEX", "112", "2146", "112_1_ALEX_1.jpg", "112_1_ALEX_2.jpg"),
+	   array("J.HAN", "65", "65_1_J.HAN_1.jpg"),
+	   
+	   
+   ),
+   array(
+       array("스내퍼", "snapper/"),
+	   array("서조사", "55", "55_1_seo.jpg"),
+	   array("서조사", "52", "2872", "52_1_seo_1.jpg", "52_1_seo_2.jpg"),
+	   array("서조사", "52", "2889", "52_2_seo_1.jpg"),
+	   array("서조사", "51", "1920", "51_1_seo_1.jpg"),
+	   array("세상끝까지", "49", "2885", "49_1_세상끝까지_1.jpg", "49_1_세상끝까지_2.jpg"),
+	   array("J.HAN", "45", "2844", "45_1_J.HAN_1.jpg"),
+	   array("Amy.K", "42", "2891", "42_1_Amy.K_1.jpg", "42_1_Amy.K_2.jpg"),
+   ),
+   array(
        array("브림", "bream/"),
        array("서조사", "44", "3034", "44_1_seo_1.jpg", "44_1_seo_2.jpg"),
        array("숀", "42.5", "2495", "42.5_1_숀_1.jpg", "42.5_1_숀_2.jpg"),
@@ -85,16 +103,6 @@ $ranking = array
 	   array("서조사", "56", "2016", "56_1_seo.jpg"),
 	   array("Duri", "53.5", "53.5_1_duri_1.jpg", "53.5_1_duri_2.jpg"),
 	   array("Amy K", "53", "53_1_amy.jpg"),
-   ),
-   array(
-       array("스내퍼", "snapper/"),
-	   array("서조사", "55", "55_1_seo.jpg"),
-	   array("서조사", "52", "2872", "52_1_seo_1.jpg", "52_1_seo_2.jpg"),
-	   array("서조사", "52", "2889", "52_2_seo_1.jpg"),
-	   array("서조사", "51", "1920", "51_1_seo_1.jpg"),
-	   array("세상끝까지", "49", "2885", "49_1_세상끝까지_1.jpg", "49_1_세상끝까지_2.jpg"),
-	   array("J.HAN", "45", "2844", "45_1_J.HAN_1.jpg"),
-	   array("Amy.K", "42", "2891", "42_1_Amy.K_1.jpg", "42_1_Amy.K_2.jpg"),
    ),
 );
 
@@ -114,6 +122,7 @@ $etc = array
 	   array("에취렌더", "레드모웡", "에취_1.jpg", "에취_2.jpg"),
 	   array("Dexter", "능성어", "2881", "Dexter_1_능성어_1.jpg", "Dexter_1_능성어_2.jpg"),
 	   array("서조사", "블랙코드", "3004", "서조사_1_블랙코드_1.jpg", "서조사_1_블랙코드_2.jpg"),
+	   array("서조사", "랍스터", "3172", "서조사_1_랍스터_1.jpg", "서조사_1_랍스터_2.jpg", "서조사_1_랍스터_3.jpg"),
     ),
 );
 ?>
